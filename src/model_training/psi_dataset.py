ec46f19704d2a36cd5c6124a6ffd4c350420c300
Yaroslav Sokolov <sokolov.yas@gmail.com>
2021-06-18 02:00:31 +0300

    Fixed bug with labels size

--- src/model_training/psi_dataset.py
+++ src/model_training/psi_dataset.py
@@ -88,8 +88,8 @@ class PSIDataset(IterableDataset):
         _, ids = res
         examples = []
         for start in range(0, len(ids), int((1 - self._overlap_slicing) * self._example_len)):
-            inp = torch.tensor(ids[start : start + self._example_len], dtype=torch.long)
             labels = torch.tensor(ids[start + 1 : start + self._example_len + 1], dtype=torch.long)
+            inp = torch.tensor(ids[start : start + labels.size(0)], dtype=torch.long)
             if self._pad_overlapped and start:
                 labels[: int(self._overlap_slicing * self._example_len)] = self._labels_pad
             examples.append((inp, labels))

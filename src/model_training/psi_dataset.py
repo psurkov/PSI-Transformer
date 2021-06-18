80cae1278be9ee35c54aa52c1ffbe58802925d9f
Yaroslav Sokolov <sokolov.yas@gmail.com>
2021-06-18 17:02:35 +0300

    Fixed DDP issue with different amount of data

--- src/model_training/psi_dataset.py
+++ src/model_training/psi_dataset.py
@@ -56,30 +56,32 @@ class PSIDataset(IterableDataset):
         worker_info = get_worker_info()
         worker_id = 0 if worker_info is None else worker_info.id
         num_workers = 1 if worker_info is None else worker_info.num_workers
-
-        if self._need_shuffle:
-            lines_iterator = iter(self._get_file_lines(worker_id, num_workers))
-            lines_bucket = True
-            while lines_bucket:
-                lines_bucket = list(itertools.islice(lines_iterator, self._shuffle_bucket))
-                examples_bucket = [example for line in lines_bucket for example in self._prepare_line(line)]
-                shuffle(examples_bucket)
-                yield from examples_bucket
-        else:
-            yield from (
-                example for line in self._get_file_lines(worker_id, num_workers) for example in self._prepare_line(line)
-            )
-
-    def _get_file_lines(self, worker_id: int, num_workers: int) -> Iterator[str]:
-        assert worker_id < num_workers
         # Assuming that num_workers are the same in the whole world
         world_size = self._world_size * num_workers
         rank = self._rank * num_workers + worker_id
 
+        yield from self._world_size_batching(rank, world_size)
+
+    def _world_size_batching(self, rank: int, world_size: int) -> Iterator[Tuple[torch.Tensor, torch.Tensor]]:
+        example_iterator = self._example_iterator()
+        while True:
+            world_batch = list(itertools.islice(example_iterator, world_size))
+            # Very important check that ensures, that every process will get the same amount of data
+            # So every process on DDP will have same amount of batches
+            if len(world_batch) != world_size:
+                break
+            yield world_batch[rank]
+
+    def _example_iterator(self) -> Iterator[Tuple[torch.Tensor, torch.Tensor]]:
+        bucket = []
         with open(self._data_path, "r") as f:
-            for i, line in enumerate(f):
-                if i % world_size == rank:
-                    yield line
+            for line in f:
+                bucket.extend(self._prepare_line(line))
+                if not self._need_shuffle or len(bucket) >= self._shuffle_bucket:
+                    shuffle(bucket)
+                    yield from (example for example in bucket)
+                    bucket = []
+        yield from bucket
 
     def _prepare_line(self, line: str) -> List[Tuple[torch.Tensor, torch.Tensor]]:
         res = self._psi_facade.transform(line, to_filter=True)
